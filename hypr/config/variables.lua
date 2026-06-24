8fd1398d5b598dbd1acd7fd13cb67a51d2289fdd
Ramon West <ramon@ramonwestsoftware.com>
2026-06-23 22:02:09 -0700

    fix webapp keybinds not launching, add flatpak run command

--- hypr/config/variables.lua
+++ hypr/config/variables.lua
@@ -16,12 +16,13 @@ local launch_resources = terminal .. " -e " .. resources
 local launch_screenshot = "cd $HOME/Pictures && " .. terminal .. " -e " .. screenshot
 
 -- flatpak packages (ensure these are installed or update with your own options)
-local messages = "org.signal.Signal"
-local webapp = "org.chromium.Chromium --app="
-local freetube = "io.freetubeapp.FreeTube"
+local messages = "flatpak run org.signal.Signal"
+local webapp = "flatpak run org.chromium.Chromium --new-window --app="
+local freetube = "flatpak run io.freetubeapp.FreeTube"
 
 -- web apps (update with your own preferred services)
--- launch webapps as "org.chromium.Chromium --app='https://youtube.com'"
+-- launch webapps as
+-- flatpak run org.chromium.Chromium --new-window --app='https://youtube.com'
 local email = webapp .. "'https://mail.proton.me/u/1/inbox'"
 local calendar = webapp .. "'https://calendar.proton.me/u/1/'"
 local passwords = webapp .. "'https://pass.proton.me/u/1'"
@@ -35,29 +36,29 @@ local bluesky = webapp .. "'https://bsky.app'"
 local discord = webapp .. "https://discord.com/channels/@me"
 
 return {
-    mods = {
-        super_shift = SUPER_SHIFT,
-        super = SUPER
-    },
-    apps = {
-        terminal = terminal,
-        menu = menu,
-        fileManager = fileManager,
-        browser = browser,
-        resources = launch_resources,
-        screenshot = launch_screenshot,
-        logout = logout,
-        messages = messages,
-        email = email,
-        calendar = calendar,
-        passwords = passwords,
-        notes = notes,
-        music = music,
-        photos = photos,
-        chatbot = chatbot,
-        youtube = youtube, -- or freetube (flatpak)
-        reddit = reddit,
-        bluesky = bluesky,
-        discord = discord,
-    }
+	mods = {
+		super_shift = SUPER_SHIFT,
+		super = SUPER,
+	},
+	apps = {
+		terminal = terminal,
+		menu = menu,
+		fileManager = fileManager,
+		browser = browser,
+		resources = launch_resources,
+		screenshot = launch_screenshot,
+		logout = logout,
+		messages = messages,
+		email = email,
+		calendar = calendar,
+		passwords = passwords,
+		notes = notes,
+		music = music,
+		photos = photos,
+		chatbot = chatbot,
+		youtube = youtube, -- or freetube (flatpak)
+		reddit = reddit,
+		bluesky = bluesky,
+		discord = discord,
+	},
 }

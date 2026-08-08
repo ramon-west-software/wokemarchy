7b60a602493957985b22c31ba0da7d84687ff827
Ramon West <ramon@ramonwestsoftware.com>
2026-08-08 15:11:15 -0700

    add external monitor context

--- hypr/config/monitors.lua
+++ hypr/config/monitors.lua
@@ -9,3 +9,19 @@ hl.monitor({
 	position = "auto",
 	scale = "auto",
 })
+
+-- Internal laptop display
+hl.monitor({
+	output = "eDP-1",
+	mode = "preferred",
+	position = "auto",
+	scale = "auto",
+})
+
+-- External monitor
+hl.monitor({
+	output = "DP-2",
+	mode = "1920x1080@75",
+	position = "0x0",
+	scale = "auto",
+})
